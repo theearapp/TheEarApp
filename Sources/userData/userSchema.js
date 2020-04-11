05ffa0d5f0ce7710db501dca112159940eb831e3
Jon Paricien <theearapp2019@gmail.com>
2020-04-11 13:23:42 -0400

    testing

--- Sources/userData/userSchema.js
+++ Sources/userData/userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; const ObjectId = Schema.ObjectId;
 // const bcrypt = require('bcryptjs');
+
 // const SALT_WORK_FACTOR = 10; // used for password encryption
 
 const userSchema = new Schema({
