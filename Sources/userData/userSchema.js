d912e062990365de2b522b2c48e3761fe0b72679
Jon Paricien <theearapp2019@gmail.com>
2020-04-11 13:10:16 -0400

    test

--- Sources/userData/userSchema.js
+++ Sources/userData/userSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; const ObjectId = Schema.ObjectId;
-const bcrypt = require('bcryptjs');
-const SALT_WORK_FACTOR = 10; // used for password encryption
+// const bcrypt = require('bcryptjs');
+// const SALT_WORK_FACTOR = 10; // used for password encryption
 
 const userSchema = new Schema({
   uid: {type: ObjectId, required: true, index: {unique: true}},
@@ -34,31 +34,31 @@ const userSchema = new Schema({
   }],
 }, {collection: 'earUser'});
 
-userSchema.pre('save', function(next) {
-  // only hash the password if it has been modified (or is new)
-  if (!this.isModified('password')) return next();
+// userSchema.pre('save', function(next) {
+//   // only hash the password if it has been modified (or is new)
+//   if (!this.isModified('password')) return next();
 
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) return next(err);
+//   // generate a salt
+//   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//     if (err) return next(err);
 
-    // hash the password using our new salt
-    bcrypt.hash(this.password, salt, (err, hash) => {
-      if (err) return next(err);
+//     // hash the password using our new salt
+//     bcrypt.hash(this.password, salt, (err, hash) => {
+//       if (err) return next(err);
 
-      // override the cleartext password with the hashed one
-      this.password = hash;
-      next();
-    });
-  });
-});
+//       // override the cleartext password with the hashed one
+//       this.password = hash;
+//       next();
+//     });
+//   });
+// });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  // console.log(candidatePassword);
-  // console.log(this.password);
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
+// userSchema.methods.comparePassword = function(candidatePassword, cb) {
+//   // console.log(candidatePassword);
+//   // console.log(this.password);
+//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// };
 module.exports = mongoose.model('earUsers', userSchema, 'earUsers');
